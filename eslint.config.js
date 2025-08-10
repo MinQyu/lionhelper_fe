6b7a4fa1e31192100eeaaaaef9d393a1f061f5ea
Min-gyu <jorushs@gmail.com>
2025-08-10 19:48:26 +0900

    [Chore] ESLint 및 Prettier 설정 업데이트 - .prettierignore 파일 추가 및 무시할 파일 목록 정의 - .prettierrc 파일에서 설정 변경: trailingComma, endOfLine 등 - eslint.config.js 파일에서 import 문 및 규칙 수정 - package.json 및 package-lock.json에 @radix-ui/react-checkbox 및 react-syntax-highlighter 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,20 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
-import prettier from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config([
-  globalIgnores(["dist"]),
+  globalIgnores(['dist']),
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
+      reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
       prettierConfig,
     ],
@@ -26,7 +26,7 @@ export default tseslint.config([
       globals: globals.browser,
     },
     rules: {
-      "prettier/prettier": "warn",
+      'prettier/prettier': 'warn',
     },
   },
 ]);
