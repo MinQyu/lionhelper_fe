06e5dd41145a3a7b8c7d5b539aab0b871c99e440
Min-gyu <jorushs@gmail.com>
2025-08-21 09:27:16 +0900

    [Refactor] 사이드바 및 부트캠프 상세 페이지 코드 정리 - 사이드바에서 사용하지 않는 useEffect 훅 제거 - 부트캠프 상세 페이지에서 주석 제거로 코드 간결화

--- src/pages/bootcamp/[bootcampId]/index.tsx
+++ src/pages/bootcamp/[bootcampId]/index.tsx
@@ -1,11 +1,7 @@
 import { useParams } from 'react-router-dom';
 
 function BootcampDetail() {
-  // ▶ useParams 훅을 사용하여 URL 매개변수에 액세스합니다.
-  //    useParams는 현재 경로의 모든 동적 매개변수를 객체 형태로 반환합니다.
-  // → 구조 분해 할당을 이용하여 바로 값을 넣습니다.
   const { BootcampId } = useParams();
-
   return <div>Bootcamp id: {BootcampId}</div>;
 }
 
