3d12d555ef03776851b10b77ba95f2efcc0d2f42
Min-gyu <jorushs@gmail.com>
2025-09-01 00:16:26 +0900

    [Refactor] 대시보드 훅 및 스토어 파일 삭제 - 사용되지 않는 useDashboard 훅 및 index.ts 스토어 파일 삭제

--- src/store/index.ts
+++
@@ -1,8 +0,0 @@
-// Store exports
-export { useBootcampStore, type Course } from './bootcampStore';
-
-// 향후 다른 store들 추가 예정
-// export { useAuthStore } from './authStore';
-// export { useUserStore } from './userStore';
-
-
